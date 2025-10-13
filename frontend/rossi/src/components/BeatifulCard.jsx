e551b4b33877c2b7addcd755e4ffa04cca44850b
Federico Giozzi <fedegiozzi@gmail.com>
2025-10-12 22:29:31 -0300

    Cambios

--- frontend/rossi/src/components/BeatifulCard.jsx
+++ frontend/rossi/src/components/BeatifulCard.jsx
@@ -104,7 +104,7 @@ async function guardarDatosEnBackendWithCellphone() {
         data_image: localStorage.getItem('data_text_image')
         
   };
-    console.log(`Estos son los ${datos}`);
+    console.log(datos);
     try{
         setHiddenLoad(!hiddenLoad)
         const response = await fetch("https://backend-v2-9f7y.onrender.com/guardar", 
