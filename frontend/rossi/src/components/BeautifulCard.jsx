fcb8a0f4b192388573849e21dd19e9e05c7922ea
Federico Giozzi <fedegiozzi@gmail.com>
2025-12-11 22:32:11 -0300

    Update fetch URLs to use ngrok for API calls

--- frontend/rossi/src/components/BeautifulCard.jsx
+++ frontend/rossi/src/components/BeautifulCard.jsx
@@ -91,7 +91,7 @@ const BeautifulForm = () => {
     const interval = setInterval(() => {
       console.log(detecFichas)
       setActualizado("Actualizando...")
-      fetch("http://localhost:5000/recibir", {
+      fetch("https://deana-inspirable-weirdly.ngrok-free.dev/recibir", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ "Query": "pedime_puntaje" })
@@ -125,7 +125,7 @@ const BeautifulForm = () => {
     console.log(datos);
     try {
       setHiddenLoad(!hiddenLoad)
-      const response = await fetch("http://localhost:5000/guardar", {
+      const response = await fetch(" https://deana-inspirable-weirdly.ngrok-free.dev/guardar", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
